2d4d45dda5edb06a9fda9d078aad72e3bde5a998
Giancarlos <giancarlos.isasi@gmail.com>
2019-03-03 01:00:59 -0500

    Use the webpack-build-notifier plugin instead of webpack-notifier

--- app/frontend/webpack/environments/development.js
+++ app/frontend/webpack/environments/development.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const WebpackNotifierPlugin = require('webpack-notifier');
+const WebpackNotifierPlugin = require('webpack-build-notifier');
 
 const base = require('./base');
 
