e27916744797b8046c778290fa25dd09d2086320
Giancarlos <giancarlos.isasi@gmail.com>
2019-03-24 00:18:12 -0500

    Create MainMenu component

---
+++ app/frontend/src/layouts/components/MainMenu/MainMenu.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { styled, css } from 'src/theme/';
+import { useUserContext } from 'src/contexts/UserContext';
+
+const Container = styled.div`
+  padding: 16px 24px;
+  border-bottom: 1px solid ${props => props.theme.colors.whiteblue};
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto;
+`;
+
+const Right = styled.div`
+  text-align: right;
+`;
+
+const MainMenu: React.FunctionComponent = () => {
+  const userContext = useUserContext();
+
+  console.log({ user: userContext.user });
+
+  return (
+    <Container>
+      <div>DevTips!</div>
+      <Right>
+        {userContext.user ? (
+          <span>Hi {userContext.user.email}</span>
+        ) : (
+          <React.Fragment>
+            <a href="/auth/signup">Signup</a>
+            <a
+              css={css`
+                margin-left: 20px;
+              `}
+              href="/auth/signin"
+            >
+              Signup
+            </a>
+          </React.Fragment>
+        )}
+      </Right>
+    </Container>
+  );
+};
+
+export default MainMenu;
