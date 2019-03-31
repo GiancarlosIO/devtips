c38755cba57a9eb2310cea99dce093776ec207ec
Giancarlos <giancarlos.isasi@gmail.com>
2019-03-31 16:02:06 -0500

    Add CREH_STATIC_URL S3 to base template

--- app/core/views.py
+++ app/core/views.py
@@ -1,5 +1,16 @@
 from django.views.generic import TemplateView
+from django.conf import settings
 
 
 class BaseView(TemplateView):
     template_name = 'react-template.html'
+    _env_vars = {
+        'STATIC_URL': settings.STATIC_URL,
+    }
+
+    def get_context_data(self):
+        context_data = {
+            'env': self._env_vars,
+        }
+
+        return context_data
